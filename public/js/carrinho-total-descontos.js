cb41289efd2dd316084ae61688ea9d6acdd0317f
Lucas Silva <lucassilva.uern@gmail.com>
2022-05-16 11:32:37 -0300

    card 3 - manipulando array temp ao invés do global

--- public/js/carrinho-total-descontos.js
+++ public/js/carrinho-total-descontos.js
@@ -2,11 +2,11 @@ const carrinho = [25.99, 27.83, 35.80, 35.80, 35.80, 44, 53.90, 25.99]
 
 const retornaDuplicados = (array) => {
     const arrayFiltrado = array.filter((valor, indice) => array.indexOf(valor) !== indice);
-    return [...new Set(arrayFiltrado)]; // [25.99, 35.80]
+    return [...new Set(arrayFiltrado)];
 }
 
 const calcularDesconto = (valor, taxa) => {
-    let desconto = valor - (valor * formatarTaxa(taxa))
+    const desconto = valor - (valor * formatarTaxa(taxa))
     return desconto;
 }
 
@@ -14,16 +14,16 @@ const formatarTaxa = (taxa) => {
     return taxa / 100;
 }
 
-
-const aplicarDesconto = (taxa) => {
-    const duplicados = retornaDuplicados(carrinho);
+const aplicarDesconto = (carrinho, taxa) => {
+    let carrinhoTemp = [...carrinho];
+    const duplicados = retornaDuplicados(carrinhoTemp);
     duplicados.forEach(valorProduto => {
-        let indice = carrinho.indexOf(valorProduto);
+        let indice = carrinhoTemp.indexOf(valorProduto);
         let desconto = calcularDesconto(valorProduto, taxa);
-        if (indice != -1) carrinho.splice(indice, 1, desconto);
+        if (indice != -1) carrinhoTemp.splice(indice, 1, desconto);
     })
 
-    console.table(carrinho)
+    return carrinhoTemp;
 }
 
 const formatarValor = (valor) => {
@@ -31,8 +31,7 @@ const formatarValor = (valor) => {
 }
 
 const totalCarrinho = () => {
-    aplicarDesconto(10);
-    const valorTotal = carrinho.reduce((anterior, atual) => anterior + atual);
+    const valorTotal = aplicarDesconto(carrinho, 10).reduce((anterior, atual) => anterior + atual);
     return formatarValor(valorTotal);
 }
 
