7a4691c9ada907eb5ece63e5ec17dc34ebb6c258
Lucas Silva <lucassilva.uern@gmail.com>
2022-05-20 10:12:24 -0300

    Modificações na página principal

---
+++ public/js/estoque.js
@@ -0,0 +1,16 @@
+const produtos = [
+    {
+        nome: 'Nome Produto',
+        url: 'www.google.com',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 10,
+        imagens: [
+            {
+                url: 'link para imagem',
+                descricao: 'camiseta lipp no tamanho p'
+            }
+        ]
+    }
+]
