56a9425f69ea218777e5ca8618f7cc4a77ce1814
Lucas Silva <lucassilva.uern@gmail.com>
2022-05-23 12:02:20 -0300

    card 1 - Criando objetos literais

---
+++ public/js/estoque.js
@@ -0,0 +1,122 @@
+export const estoque = [{
+        id: 1,
+        nome: 'Camisa Cropped Feminina com Linho Animal Print Verde AK by Riachuelo',
+        url: 'www.google.com',
+        preco: 139.90,
+        descricao: 'Clássica e atemporal, mas repaginada, a Camisa Cropped Feminina com Linho Animal Print Verde AK by Riachuelo é a escolha ideal para looks estilosos e fashionistas! Confeccionada em viscose e linho, a peça apresenta modelagem cropped e padronagem de zebra. Combine com calça pantalona e tênis para um visual despojado e fashionista!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 10,
+        imagens: [{
+            url: 'public/images/products/lancamento-1.png',
+            descricao: 'Imagem grande de uma Camisa Cropped Feminina'
+        }],
+        thumbnail: [{
+                url: 'public/images/products/lancamento-1.png',
+                descricao: 'Imagem pequena de uma Camisa Cropped Feminina'
+            },
+            {
+                url: 'public/images/products/lancamento-1-part2.png',
+                descricao: 'Imagem pequena de uma Camisa Cropped Feminina'
+            },
+            {
+                url: 'public/images/products/lancamento-1-part3.png',
+                descricao: 'Imagem pequena de uma Camisa Cropped Feminina'
+            },
+            {
+                url: 'public/images/products/lancamento-1-part4.png',
+                descricao: 'Imagem pequena de uma Camisa Cropped Feminina'
+            }
+        ]
+    },
+    {
+        id: 2,
+        nome: 'Blusa Cropped Feminina Manga Longa Stranger Things Bege',
+        url: 'www.google.com',
+        preco: 139.90,
+        descricao: 'Enfrente o Demogorgon com a Blusa Cropped Feminina Manga Longa Stranger Things Bege ! Confeccionada em malha de algodão e poliéster, a peça vai deixar seus looks ainda mais estilosos e divertidos! Experimente combinar com short jeans e tênis casual para um look incrível!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 5,
+        imagens: [{
+            url: 'public/images/products/lancamento-2.png',
+            descricao: 'Blusa Cropped Feminina Manga Longa Stranger Things Bege'
+        }],
+        thumbnail: [{
+                url: 'public/images/products/lancamento-2.png',
+                descricao: 'Imagem pequena de uma Blusa Cropped Feminina Manga Longa Stranger Things Bege'
+            },
+            {
+                url: 'public/images/products/lancamento-2-part2.png',
+                descricao: 'Imagem pequena de uma Blusa Cropped Feminina Manga Longa Stranger Things Bege'
+            },
+            {
+                url: 'public/images/products/lancamento-2-part3.png',
+                descricao: 'Imagem pequena de uma Blusa Cropped Feminina Manga Longa Stranger Things Bege'
+            },
+            {
+                url: 'public/images/products/lancamento-2-part4.png',
+                descricao: 'Imagem pequena de uma Blusa Cropped Feminina Manga Longa Stranger Things Bege'
+            }
+        ]
+    },
+    {
+        id: 3,
+        nome: 'Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool by Riachuelo',
+        url: 'www.google.com',
+        preco: 199.90,
+        descricao: 'O Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool by Riachuelo é característico por seus recortes com efeitos de babados, ideal para compor looks com um toque romântico. Cheio de conforto, é confeccionado em malha com mangas 3/4, uma ótima pedida para os dias mais quentes. O modelo possui detalhe de recortes, que leva mais movimento ao look. Inspire-se!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 7,
+        imagens: [{
+            url: 'public/images/products/lancamento-3.png',
+            descricao: 'imagem grande Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool'
+        }],
+        thumbnail: [{
+                url: 'public/images/products/lancamento-3.png',
+                descricao: 'Imagem pequena de Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool'
+            },
+            {
+                url: 'public/images/products/lancamento-3-part2.png',
+                descricao: 'Imagem pequena de Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool'
+            },
+            {
+                url: 'public/images/products/lancamento-3-part3.png',
+                descricao: 'Imagem pequena de Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool'
+            },
+            {
+                url: 'public/images/products/lancamento-3-part4.png',
+                descricao: 'Imagem pequena de Vestido Midi Feminino Evasê Uma Maria Floral Preto Pool'
+            }
+        ]
+    },
+    {
+        id: 4,
+        nome: 'Saia Midi Feminina Evasê Floral Preto AK by Riachuelo',
+        url: 'www.google.com',
+        preco: 159.90,
+        descricao: 'A Saia Midi Feminina Evasê Floral Preto AK by Riachuelo é confeccionada em malha com padronagem floral. Com cós amplo e cordão para amarração, experimente combinar com a uma regata e crie um visual elegante. Aposte!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 3,
+        imagens: [{
+            url: 'public/images/products/lancamento-4.png',
+            descricao: 'imagem grande Saia Midi Feminina Evasê Floral Preto'
+        }],
+        thumbnail: [{
+                url: 'public/images/products/lancamento-4.png',
+                descricao: 'Imagem pequena de Saia Midi Feminina Evasê Floral Preto'
+            },
+            {
+                url: 'public/images/products/lancamento-4-part2.png',
+                descricao: 'Imagem pequena de Saia Midi Feminina Evasê Floral Preto'
+            },
+            {
+                url: 'public/images/products/lancamento-4-part3.png',
+                descricao: 'Imagem pequena de Saia Midi Feminina Evasê Floral Preto'
+            },
+            {
+                url: 'public/images/products/lancamento-4-part4.png',
+                descricao: 'Imagem pequena de Saia Midi Feminina Evasê Floral Preto'
+            }
+        ]
+    },
+
+]
